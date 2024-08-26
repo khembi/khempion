e31285cf06204ebf27503e16aeaa683ffeed1d21
khembi <khem.tsogt@gmail.com>
2024-08-26 22:09:40 +1000

    QACoreServiceProvider removed

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\QACoreServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
